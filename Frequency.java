f841f1a7e71a93b087f444ffe2d2822b67a33868
yuvrajkrishna <yuvrajkrishna26@gmail.com>
2026-09-14 19:33:11 +0530

    frequency bruteforce 2

--- Frequency.java
+++ Frequency.java
@@ -3,34 +3,26 @@ public class Frequency {
 
         String str = "hello how are you";
 
-        for(int i = 0; i < str.length(); i++) {
+        char[] ch = str.toCharArray();
+        boolean[] visited = new boolean[ch.length];
 
-            if(str.charAt(i) == ' ') {
+        for(int i = 0; i < ch.length; i++) {
+
+            if(ch[i] == ' ' || visited[i]) {
                 continue;
             }
 
-            boolean alreadyCounted = false;
+            int count = 1;
 
-            for(int k = 0; k < i; k++) {
-                if(str.charAt(k) == str.charAt(i)) {
-                    alreadyCounted = true;
-                    break;
-                }
-            }
+            for(int j = i + 1; j < ch.length; j++) {
 
-            if(alreadyCounted) {
-                continue;
-            }
-
-            int count = 0;
-
-            for(int j = 0; j < str.length(); j++) {
-                if(str.charAt(i) == str.charAt(j)) {
+                if(ch[i] == ch[j]) {
                     count++;
+                    visited[j] = true;
                 }
             }
 
-            System.out.println(str.charAt(i) + " = " + count);
+            System.out.println(ch[i] + " = " + count);
         }
     }
 }
