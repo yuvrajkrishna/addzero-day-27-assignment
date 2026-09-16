5b24ca651c16c38e0969edcb9c4fa41ddddb0a00
yuvrajkrishna <yuvrajkrishna26@gmail.com>
2026-09-16 10:23:49 +0530

    optmized

--- checkifallwordsarepalindrome.java
+++ checkifallwordsarepalindrome.java
@@ -1,30 +1,33 @@
 public class checkifallwordsarepalindrome {
     public static void main(String[] args) {
-        String str = "madaim eye nitin";
-        String words[] = str.split(" ");
-        for(int i = 0 ; i < words.length ; i++){
-            boolean result = isPalindrome(words[i]);
-            if(result == false){
-                System.out.println("Not palindrome");
-                return;
-            }
-        }
-        System.out.println("Palindrome");
-    }
+         String str = "madam eye nitin";
 
-    public static boolean isPalindrome(String word){
+        char[] ch = str.toCharArray();
+
         int left = 0;
-        int right = word.length()-1;
-        while(left < right){
-            if(word.charAt(left) == word.charAt(right)){
-                left++;
-                right--;
+
+        for(int i = 0; i <= ch.length; i++) {
+
+            if(i == ch.length || ch[i] == ' ') {
+
+                int right = i - 1;
+
+                while(left < right) {
+
+                    if(ch[left] != ch[right]) {
+                        System.out.println("Not palindrome");
+                        return;
+                    }
+
+                    left++;
+                    right--;
+                }
+
+                // Next word ka starting point
+                left = i + 1;
             }
-            else{
-                return false;
-            } 
-            
         }
-        return true;
+
+        System.out.println("Palindrome");
     }
 }
