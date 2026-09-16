fd7721a6fbd3f1b2215122c2b3e85dfee226af5e
yuvrajkrishna <yuvrajkrishna26@gmail.com>
2026-09-16 08:54:56 +0530

    optimized

--- checkifeqdigchars.java
+++ checkifeqdigchars.java
@@ -1,16 +1,14 @@
-public class checkifeqchar {
+public class CheckIfEqDigChars {
     public static void main(String[] args) {
 
         String str = "he0llo1234";
 
-        char[] ch = str.toCharArray();
-
         int count = 0;
         int digcount = 0;
 
-        for(int i = 0; i < ch.length; i++) {
+        for(int i = 0; i < str.length(); i++) {
 
-            if(Character.isDigit(ch[i])) {
+            if(Character.isDigit(str.charAt(i))) {
                 digcount++;
             }
             else {
