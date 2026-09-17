2da8cc1f37f8068022c5b7c9474d62308b1e97bf
yuvrajkrishna <yuvrajkrishna26@gmail.com>
2026-09-17 19:41:17 +0530

    program

---
+++ program.java
@@ -0,0 +1,8 @@
+public class program {
+    public static void main(String[] args) {
+        String str = "PROGRAM";
+        for(int i = 0 ; i < str.length(); i++){
+            System.out.println(str.substring(0,i+1));
+        }
+    }
+}
